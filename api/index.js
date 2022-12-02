d913f44a2763416ff96f417139a295ec272196ac
Diego Carlino <37153329+devteapot@users.noreply.github.com>
2022-12-02 18:55:31 +0100

    added cache control

--- api/index.js
+++ api/index.js
@@ -8,18 +8,22 @@ const getStatusCodesInRange = (from, length) =>
   [...Array(length).keys()].map(offset => from + offset)
 
 app.get('/api/1xx', (_, res) => {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(getStatusCodesInRange(100, 2).randomElement()).end();
 });
 
 app.get('/api/2xx', (_, res) => {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(getStatusCodesInRange(200, 7).randomElement()).end();
 });
 
 app.get('/api/3xx', (_, res) => {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(getStatusCodesInRange(300, 9).randomElement()).end();
 });
 
 app.get('/api/4xx', (_, res) => {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status([
     ...getStatusCodesInRange(400, 19),
     ...getStatusCodesInRange(421, 2),
@@ -28,6 +32,7 @@ app.get('/api/4xx', (_, res) => {
 });
 
 app.get('/api/5xx', (_, res) => {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(getStatusCodesInRange(500, 6).randomElement()).end();
 });
 
