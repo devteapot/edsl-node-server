9622f752259a7d4102699466bc80730d8a1053fc
Diego Carlino <37153329+devteapot@users.noreply.github.com>
2022-12-02 18:49:58 +0100

    init

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+
+Array.prototype.randomElement = function () {
+  return this[Math.floor((Math.randomElement() * this.length))];
+}
+
+const getStatusCodesInRange = (from, length) =>
+  [...Array(length).keys()].map(offset => from + offset)
+
+app.get('/api/1xx', (_, res) => {
+  res.status(getStatusCodesInRange(100, 2).randomElement()).end();
+});
+
+app.get('/api/2xx', (_, res) => {
+  res.status(getStatusCodesInRange(200, 7).randomElement()).end();
+});
+
+app.get('/api/3xx', (_, res) => {
+  res.status(getStatusCodesInRange(300, 9).randomElement()).end();
+});
+
+app.get('/api/4xx', (_, res) => {
+  res.status([
+    ...getStatusCodesInRange(400, 19),
+    ...getStatusCodesInRange(421, 2),
+    426
+  ].randomElement()).end();
+});
+
+app.get('/api/5xx', (_, res) => {
+  res.status(getStatusCodesInRange(500, 6).randomElement()).end();
+});
+
+module.exports = app;
